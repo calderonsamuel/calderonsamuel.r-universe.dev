51942afc8fe13f1e0457939227a09669f787de8f
Samuel Calderon <samuel.calderon@uarm.pe>
2022-07-25 16:44:40 -0500

    add transpaeconomica

--- script.R
+++ script.R
@@ -2,7 +2,8 @@ df <- tibble::tribble(
   ~package, ~subdir, ~url,
   "perumapas", NA, "https://github.com/calderonsamuel/perumapas",
   "perupobreza2018", NA, "https://github.com/calderonsamuel/perupobreza2018",
-  "templatesRmd", NA, "https://github.com/calderonsamuel/templatesRmd"
+  "templatesRmd", NA, "https://github.com/calderonsamuel/templatesRmd",
+  "transpaeconomica", NA, "https://github.com/calderonsamuel/transpaeconomica"
 )
 
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
