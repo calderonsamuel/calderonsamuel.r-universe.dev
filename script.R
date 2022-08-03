910625dc529a600a255b3bb6452f524a4893e23b
Samuel Calderon <samuel.calderon@uarm.pe>
2022-08-03 08:12:38 -0500

    fix typo

--- script.R
+++ script.R
@@ -3,7 +3,7 @@ df <- tibble::tribble(
   "perumapas", NA, "https://github.com/calderonsamuel/perumapas",
   "perupobreza2018", NA, "https://github.com/calderonsamuel/perupobreza2018",
   "templatesRmd", NA, "https://github.com/calderonsamuel/templatesRmd",
-  "transpaeconomica", NA, "https://github.com/calderonsamuel/transpaeconomica"
+  "transpaeconomica", NA, "https://github.com/calderonsamuel/perutranspaeconomica"
 )
 
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
