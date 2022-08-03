f5cacd01c100a15bb63f1b1f307525ba448417bb
Samuel Calderon <samuel.calderon@uarm.pe>
2022-08-03 08:13:55 -0500

    oops

--- script.R
+++ script.R
@@ -3,6 +3,6 @@ tibble::tribble(
   "perumapas", NA, "https://github.com/calderonsamuel/perumapas",
   "perupobreza2018", NA, "https://github.com/calderonsamuel/perupobreza2018",
   "templatesRmd", NA, "https://github.com/calderonsamuel/templatesRmd",
-  "transpaeconomica", NA, "https://github.com/calderonsamuel/perutranspaeconomica"
+  "perutranspaeconomica", NA, "https://github.com/calderonsamuel/perutranspaeconomica"
 ) |> 
   jsonlite::write_json('packages.json', pretty = TRUE)
