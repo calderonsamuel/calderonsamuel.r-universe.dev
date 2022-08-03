b480f6b66eec1b0f913ea6f054863658d0a18cfe
Samuel Calderon <samuel.calderon@uarm.pe>
2022-08-03 08:13:14 -0500

    pipe ftw

--- script.R
+++ script.R
@@ -1,9 +1,8 @@
-df <- tibble::tribble(
+tibble::tribble(
   ~package, ~subdir, ~url,
   "perumapas", NA, "https://github.com/calderonsamuel/perumapas",
   "perupobreza2018", NA, "https://github.com/calderonsamuel/perupobreza2018",
   "templatesRmd", NA, "https://github.com/calderonsamuel/templatesRmd",
   "transpaeconomica", NA, "https://github.com/calderonsamuel/perutranspaeconomica"
-)
-
-jsonlite::write_json(df, 'packages.json', pretty = TRUE)
+) |> 
+  jsonlite::write_json('packages.json', pretty = TRUE)
